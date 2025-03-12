4a312a4baa3f0b7bff2205026212216255347a2c
Mattia Lo Mele <mattialomele@MacBook-Air-di-Mattia.local>
2025-03-12 11:20:36 +0100

    Added Exercise1 and Exercise2

---
+++ Exercise1/main.cpp
@@ -0,0 +1,26 @@
+#include <fstream> // stream per i file
+#include <iostream>
+
+int main()
+{
+    std::ifstream file("data.txt"); // crea un oggetto che possa leggere il file e legalo a data.txt
+    // I file hanno uno stato positivo o negativo -> negativo se operazione non a buon fine -> non posso eseguirne altre
+    if(file.fail())
+    {
+        std::cerr << "Non è stato possibile aprire il file" << std::endl;
+        return 1; // return 1 per segnalare errore
+    }
+
+    std::string tmp;
+    std::getline(file, tmp); // legge la linea del file e la salva in var tmp
+    std::cout << tmp << std::endl;
+    
+    int val;
+    int sum = 0;
+    while(file >> val)
+    {
+        sum = sum + val * val; // nel momento in cui arriva al carattere end of line, file >> val diventa falso perché non ha senso, quindi si esce dal while
+    }
+    
+    return 0;
+}
